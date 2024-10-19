f9ca297f2d5d0e5d28dc7173076ce7d9cd2735a5
Shuvkant <weird426344@gmail.com>
2024-10-19 21:12:16 +0545

    understand the react flow

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+  <h1>Hello, Shuvkant Chaudhary Phanait</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h2>Hello | Shuvkant Phanait</h2>;
+}
+
+export default App;
