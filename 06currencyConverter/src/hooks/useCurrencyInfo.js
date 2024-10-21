a741b95ab7580b01f82ae12c840c307db366f687
Shuvkant <weird426344@gmail.com>
2024-10-21 19:05:40 +0545

    06currencyconverter second commit

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
@@ -6,7 +6,9 @@ function useCurrencyInfo(currency) {
     fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`,
     ).then((res) => res.json()).then((res) => setData(res[currency]));
+    console.log(data);
   }, [currency]);
+  console.log(data);
   return data;
 }
-
+export default useCurrencyInfo;
