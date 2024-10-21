158d92db894c0a3c26c501050a382af376a3a7ab
Shuvkant <weird426344@gmail.com>
2024-10-21 20:42:32 +0545

    07 react Router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="pd-4 bg-teal-500 ">
+        React Router || Shuvkant
+      </h3>
+    </>
+  );
+}
+
+export default App;
