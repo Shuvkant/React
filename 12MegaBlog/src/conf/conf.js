97057c21078d196b9a0f2688f666160c98eea6da
Shuvkant <weird426344@gmail.com>
2024-10-25 09:01:51 +0545

    configure redux toolkit

--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,11 +1,9 @@
-import { string } from "../../node_modules/@types/prop-types/index.d.ts";
-
 const conf = {
-  appwriteUrl: string(import.meta.env.VITE_APPWRITE_URL),
-  appwriteProjectId: string(import.meta.env.VITE_APPWRITE_URL),
-  appwriteDatabaseid: string(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwriteCollectionId: string(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  appwriteBucketId: string(import.meta.env.VITE_APPWRITE_BUCKER_ID),
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteDatabaseid: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKER_ID),
 };
 
 export default conf;
