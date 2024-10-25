aac8822ce3c319de6259e6738d88057909766550
Shuvkant <weird426344@gmail.com>
2024-10-25 10:22:35 +0545

    production grade react components

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
+import Container from "./container/Container.jsx";
+import Logo from "./Logo.jsx";
+import loguotBtn from "./Header/LogoutBtn.jsx";
 
-export { Footer, Header };
+export { Container, Footer, Header, Logo, loguotBtn };
